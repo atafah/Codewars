6671c1d9a37f469091331eb64f2c2a8414f563b3
anas tafah <154932472+atafah@users.noreply.github.com>
2025-09-02 02:26:07 +0100

    Add solution - CodeHub

---
+++ 8-kyu/opposites-attract/opposites-attract.py
@@ -0,0 +1,6 @@
+def lovefunc( flower1, flower2 ):
+    if (flower1 % 2 == 0 and flower2 % 2 == 1):
+        return True
+    elif (flower2 % 2 == 0 and flower1 % 2 == 1):
+        return True
+    return False
