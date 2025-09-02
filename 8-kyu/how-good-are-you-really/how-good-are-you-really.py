d6f9bcde47e2994d275c92352b5e1289b063617d
anas tafah <154932472+atafah@users.noreply.github.com>
2025-09-02 02:29:34 +0100

    Add solution - CodeHub

---
+++ 8-kyu/how-good-are-you-really/how-good-are-you-really.py
@@ -0,0 +1,8 @@
+def better_than_average(class_points, your_points):
+    sum = 0
+    for point in class_points:
+        sum += point
+        
+    sum += your_points
+    avg = sum / (len(class_points)+1)
+    return your_points > avg
