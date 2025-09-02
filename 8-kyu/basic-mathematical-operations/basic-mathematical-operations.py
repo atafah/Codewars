c637cd8183d266e2c7719d887c32189ed90c55ed
anas tafah <154932472+atafah@users.noreply.github.com>
2025-09-02 02:31:13 +0100

    Add solution - CodeHub

---
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.py
@@ -0,0 +1,10 @@
+def basic_op(operator, value1, value2):
+    p = operator
+    if (p == '+'):
+        return value1 + value2
+    elif (p == '-'):
+        return value1 - value2
+    elif (p == '*'):
+        return value1 * value2
+    elif (p == '/'):
+        return value1 / value2
